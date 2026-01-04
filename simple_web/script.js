437ed43b66ecb6de7b86b0f35c7aa7b4c7c4b646
basii-web <basi.kj.143@gmail.com>
2026-01-04 23:08:35 +0530

    Add files via upload

---
+++ simple_web/script.js
@@ -0,0 +1,21 @@
+function scrollToSection(id)
+{
+  document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+}
+
+function submitForm()
+{
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+
+  if (name && email && message)
+    {
+    document.getElementById("form-status").innerText =
+      `Thanks ${name}! Your message has been sent 🚀`;
+  } else {
+    document.getElementById("form-status").innerText =
+      "Please fill out all fields.";
+  }
+  return false;
+}
